7a68600e89a32e838602594622678c85820b5cea
Dibbo Das Avi <dibbodasavi@gmail.com>
2025-05-20 01:39:21 +0600

    Create missing page components

---
+++ src/pages/Papers.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Papers: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Papers</h1>
+    </div>
+  );
+};
+
+export default Papers;
---
+++ src/pages/Photography.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Photography: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Photography</h1>
+    </div>
+  );
+};
+
+export default Photography;
